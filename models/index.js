9b753073f52ee5f786b7669febf2bec3f4d07720
Layla Hammmoud <laylahammoud63@gmail.com>
2024-05-25 13:04:22 +0300

    finish the set up of the project and the connection to the database

---
+++ models/index.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config.js";
+
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+const db = {
+    sequelize,
+    testConnection
+}
+
+export default db
